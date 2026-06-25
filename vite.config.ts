8f9426e410f7a143c4520d24e68a5aaad262183e
Naman Agarwal <145967357+NamanAgarwal15@users.noreply.github.com>
2026-06-26 03:44:02 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/naman-animated-folio/",
+  base: '/naman-s-animated-portfolio-54/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
